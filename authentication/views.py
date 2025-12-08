541be1caff7644f5577100b36d8e585e60907cb2
Noam <noamguez0@gmail.com>
2025-12-08 14:57:15 +0100

    Add authentification config and other apps

---
+++ authentication/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def login_view(request):
+    return render(request, "authentication/login.html")
+
+
+def logout_view(request):
+    return render(request, "authentication/logout.html")
